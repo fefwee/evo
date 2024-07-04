5f539616124505bbdc83f431b3c0efe97256450b
Anton Berger <aweo.wew@mail.ru>
2024-07-04 23:19:31 +0300

    добавил сервис для получения данных

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './components/book/book.component';
-import {BooksService} from "./books.service";
+import {BooksService} from "./services/books.service";
 
 @NgModule({
   declarations: [
